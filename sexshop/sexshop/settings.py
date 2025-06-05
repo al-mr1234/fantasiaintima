36320eb565eb67bfb8606e5e88c48c7dcf6c07c7
Alejandra <alejamarin2531@gmail.com>
2025-06-05 10:49:48 -0500

    revierto el commit del perto en settingd

--- sexshop/sexshop/settings.py
+++ sexshop/sexshop/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3307',
+        'PORT': '3306',
         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
     }
 }
